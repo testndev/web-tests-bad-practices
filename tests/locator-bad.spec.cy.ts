3b6e82c15ba8a2c4ec0c9760d32568fab62e8cb8
Alhusaine NEMER <al@testndev.com>
2025-04-11 16:56:37 +0200

    feat: cypress tests are executables

--- tests/locator-bad.spec.cy.ts
+++ tests/locator-bad.spec.cy.ts
@@ -5,7 +5,7 @@ describe("Tests Cypress", () => {
   it("Select an option, click on the filter button and see the result", () => {
     cy.visit("/dashboard")
 
-    cy.get('#main_panel .filter form #name').input("my_username");
+    cy.get('#main_panel .filter form #name').type("my_username");
     cy.get('#main_panel .filter form .submit').click();
 
     cy.get('#result_panel .mat-table .mat-row').should("have.length", 10)
--- tests/locator-good.spec.cy.ts
+++ tests/locator-good.spec.cy.ts
@@ -5,7 +5,7 @@ describe("Tests Cypress", () => {
   it("Select an option, click on the filter button and see the result", () => {
     cy.visit("/dashboard")
 
-    cy.get('[data-cy="dashboard_filter_username"]').input("my_username");
+    cy.get('[data-cy="dashboard_filter_username"]').type("my_username");
     cy.get('[data-cy="dashboard_filter_button"]').click();
 
     cy.get('[data-cy="dashboard_result_row"]').should("have.length", 10)
