34772ee5949b908fa1f3a0e371ef5e6ad9235b36
Alain <bluepioupiou@gmail.com>
2025-04-14 14:15:59 +0200

    ajout du chaining

--- tests/chaining-good.spec.cy.ts
+++ tests/chaining-good.spec.cy.ts
@@ -1,16 +1,14 @@
-// Example Cypress : bad practice 👎
+// Example Cypress : bad practice 👍
 
 
 describe("Test the login", () => {
 
   it("Should fill the name and password inputs and click on submit", () => {
     cy.visit("https://the-internet.herokuapp.com/login")
-      .get("#username")
-      .type("tomsmith")
-      .get("#password")
-      .type("SuperSecretPassword!")
-      .get("button[type='submit']")
-      .click();
+
+    cy.get("#username").type("tomsmith")
+    cy.get("#password").type("SuperSecretPassword!")
+    cy.get("button[type='submit']").click();
 
     cy.contains("Secure Area").should("be.visible")
   })
