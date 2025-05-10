5df735e441d23e8696dcb6a369e25de77ad84fad
Alhusaine NEMER <al@testndev.com>
2025-05-11 01:09:43 +0200

    refactor: adjust indentation

--- tests/10-web-assertions.spec.ts
+++ tests/10-web-assertions.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from "@playwright/test";
 import CoffeePage from "./pages/coffee-pw.page";
 
 test(`Nine coffees should be visible`, async ({ page }) => {
-      const coffeePage = await new CoffeePage(page).goto('slow');
+  const coffeePage = await new CoffeePage(page).goto('slow');
   await expect(coffeePage.getCoffees()).toHaveCount(9);
 });
